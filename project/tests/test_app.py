d5fa33e68c097a4d23309b9a42ffaf74b1430672
Guyokps <124910053+Guyokps@users.noreply.github.com>
2023-07-02 20:11:36 +0300

    testing ci process

---
+++ project/tests/test_app.py
@@ -0,0 +1,18 @@
+import unittest
+from flask import Flask
+
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
+
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+
+
+if __name__ == '__main__':
+    unittest.main()
